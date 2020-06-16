d89567a745b72bf213ce4d67fa645985f75beb2f
Wu Lucas <lucas12121992@gmail.com>
2020-06-16 18:05:51 -0300

    Pro version

--- password.py
+++ password.py
@@ -1,12 +1,14 @@
 password = 'a123456'
 x = 3  # 剩餘機會
-while True:
+while x > 0:
+    x = x - 1
     pw = input('enter the password ')
     if pw == password:
         print('Login successfully')
         break
     else:
-        x = x - 1
-        print('Incorrect! You left', x, 'chance')
-        if x == 0:
-            break 
+        print('Incorrect!')
+        if x > 0:
+            print('You left', x, 'chance')
+        else:
+            print('You dont have more chance!')
